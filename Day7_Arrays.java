4c9154b3bb50d5be4b248f9e3fffe561fdda87c6
RAljowair <rayanaljowair@gmail.com>
2021-03-14 21:59:23 +0300

    Day7_Arrays.java SOLVED!

--- Day7_Arrays.java
+++ Day7_Arrays.java
@@ -26,6 +26,10 @@ public class Solution {
             arr[i] = arrItem;
         }
 
+        for (int i = arr.length-1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+
         scanner.close();
     }
 }
